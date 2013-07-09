e76c46b706e3bc1db30bb70b4bfc51fad52e4d2a
Aaron Morais <aaron.m@live.ca>
2013-07-09 00:43:00 -0400

    Update hover script to parse hidden panel link and make the panel appear on hover.

--- hover.js
+++ hover.js
@@ -1,3 +1,39 @@
 $(document).ready(function() {
-	console.log("Hello world!");
+    console.log("HELLO WORLD!!!!");
+
+    //get the bonus link for the bonus panel
+    var href = $("#extrapanelbutton > a").attr("href");
+    var hrefSplitArray = href.split("/");
+    var date = hrefSplitArray[hrefSplitArray.length-2];
+    var bonusLink = "http://invisiblebread.com/eps/" + date.substring(0,4) + "-" + date.substring(4,6) + "-" + date.substring(6) + "-extrapanel.png";
+
+    //reset the extra panel container
+    $("#extrapanelbutton").html("");
+
+    //add box to hover over to bonus panel
+    var box = $("<a>");
+    $("#extrapanelbutton").append(box);
+
+    //add hover image to bonus panel
+    var img = $("<img>");
+    img.attr("src", bonusLink);
+    img.attr("id", "hoverImg");
+    img.attr("style", "display: none;");
+    $("#extrapanelbutton").append(img);
+
+    //display hover image when hovering over box
+    $("#extrapanelbutton > a").hover(
+        function() {
+            $("#hoverImg").css("display", "inline");
+            $("#extrapanelbutton > a").css("display", "none");
+        }
+    );
+
+    $("#hoverImg").mouseout(
+        function() {
+            $("#hoverImg").css("display", "none");
+            $("#extrapanelbutton > a").css("display", "block");
+        }
+    );
+
 });
